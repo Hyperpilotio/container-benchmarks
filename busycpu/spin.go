ece461cefd4d9f092a05db84a4eb36a19c5bc468
Timothy Chen <tnachen@gmail.com>
2016-12-19 04:53:50 +0000

    Print message when started

--- busycpu/spin.go
+++ busycpu/spin.go
@@ -1,8 +1,13 @@
 package main
 
+import (
+	"fmt"
+)
+
 // We use this to occupy all the cpu shares given to this container.
 // Not looking to introduce any other resource interference.
 func main() {
+	fmt.Println("Starting to spin....")
 	for {
 	}
 }
