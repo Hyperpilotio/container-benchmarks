e2b20162e80f69290dda08ce1c628092df51a300
chyeh <chyeh@cepave.com>
2017-06-03 19:03:50 +0800

    [#1] Use `main.go`

--- benchmark-agent/agent.go
+++ benchmark-agent/agent.go
@@ -244,23 +244,3 @@ func (server *Server) Run() error {
 
 	return router.Run(":" + server.Port)
 }
-
-func main() {
-	endpoint := "unix:///var/run/docker.sock"
-	client, err := docker.NewClient(endpoint)
-	if err != nil {
-		panic(err)
-	}
-
-	err = client.Ping()
-	if err != nil {
-		glog.Error("Unable to ping docker daemon")
-		panic(err)
-	}
-
-	server := NewServer(client, "7778")
-	err = server.Run()
-	if err != nil {
-		panic(err)
-	}
-}
